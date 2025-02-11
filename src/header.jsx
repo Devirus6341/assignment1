ae8b081acf291bf0c7260f199cffe7682f4b6d39
Devirus6341 <devirus101@gmail.com>
2025-02-11 18:56:41 +0100

    fixed image

--- src/header.jsx
+++ src/header.jsx
@@ -3,12 +3,12 @@
 function Header() {
 return <header>
         <div className="Header-left" >
-            <p>Home</p>
+            <a>Home</a>
             </div>
             <div className="Header-right">
-            <p>Introduction</p>
-            <p>Case Study</p>
-            <p>Conclusion</p>
+            <a>Introduction</a>
+            <a>Case Study</a>
+            <a>Conclusion</a>
         </div>
         </header>
 }
